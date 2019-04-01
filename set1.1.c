f3a9dc222e2659907d4df911375d13c1d4a34eba
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-04-01 15:42:27 +0530

    Create set1.1.c

---
+++ set1.1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,a[100],k=0,c=0,t,b[100],i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                b[k]=a[i];
+                a[j]='&';
+                k++;
+                c++;
+            }
+        }
+    }
+    if(c>0)
+    {
+    for(i=0;i<k;i++)
+    {
+        for(j=i+i;j<k;j++)
+        {
+            if(b[i]>b[j])
+            {
+                t=b[i];
+                b[i]=b[j];
+                b[j]=t;
+            }
+        }
+    }
+    
+    for(i=0;i<k;i++)
+    {
+        printf("%d ",b[i]);
+    }
+    }
+    else
+    {
+        printf("unique");
+    }
+    getch();
+}
