4c92eee2bb2932e97413c016d03c0b4b24138ed3
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-04-03 15:59:45 +0530

    Create set1.6.c

---
+++ set1.6.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[100],i,j,c=0,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+            }
+        }
+        if(c>0)
+        {
+            printf("%d",a[i]);
+            break;
+        }
+    }
+    if(c==0)
+    {
+        printf("unique");
+    }
+    getch();
+}
