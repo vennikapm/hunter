a0152fd107bcf742abc0731ebb050fe2130b5c2b
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-04-03 15:35:14 +0530

    Create set1.4.c

---
+++ set1.4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100],i,j,n,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+            }
+        }
+        if(c==0)
+        {
+            printf("%d",a[i]);
+            break;
+        }
+    }
+    return 0;
+}
